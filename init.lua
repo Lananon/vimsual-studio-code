9095f4bcb151111ab0f676764f291677539f2c25
Lana <lananon1807@gmail.com>
2026-02-14 09:24:12 +0100

    yeahg changed some fuckass shit

--- init.lua
+++ init.lua
@@ -2,12 +2,15 @@ require("config.lazy")
 require("config.keymaps")
 
 
-vim.cmd("startinsert")
-
 require("catppuccin").setup({
 	flavour = "mocha"})
 vim.cmd.colorscheme "catppuccin"
 
 vim.wo.number = true
 
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
